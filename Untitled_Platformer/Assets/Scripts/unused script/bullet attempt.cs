969cdd18929d57793e5603d11e04184554e85920
Colincro <143215600+Colincro@users.noreply.github.com>
2024-07-29 14:21:49 -0700

    Shooting kill field bug fixed

---
+++ Untitled_Platformer/Assets/Scripts/unused script/bullet attempt.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public Rigidbody2D bulletRb;
+
+    void Start()
+    {
+        bulletRb.velocity = transform.right * speed;
+    }
+    void OnTriggerEnter2D (Collider2D collide)
+    {
+        Destroy(gameObject);
+    }
+}
+
