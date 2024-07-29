e5e4af2d756c4c620cbc69892857ad36e0aca034
Deaddy2Tap <150291293+Deaddy2Tap@users.noreply.github.com>
2024-07-29 18:12:24 -0500

    Added a Start Screen

---
+++ Untitled_Platformer/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI; // Add this for the Button component
+
+public class StartScreen : MonoBehaviour
+{
+    public string sceneName;
+
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(LoadScene);
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Compile");
+    }
+}
